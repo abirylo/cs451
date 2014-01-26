ff12264fbc0b2d35e0a1793c67ae7b8d38534d5b
Dan Hitchings <DannMan999@gmail.com>
2014-01-26 15:56:03 -0600

    BenchMem now copies 200MB sequentially, no threading.

--- Makefile
+++ Makefile
@@ -1,19 +1,20 @@
-CC = g++
-CFLAGS = -c -Wall -std=c++11
+CC = clang++
+CFLAGS = -c -Wall
+THREAD = -lpthread -std=c++11
 
 all: cpu mem
 
 cpu: benchCPU.o
-	$(CC) benchCPU.o -o benchCPU -lpthread
+	$(CC) benchCPU.o -o benchCPU $(THREAD)
 
 benchCPU.o: benchCPU.cpp
-	$(CC) $(CFLAGS) benchCPU.cpp
+	$(CC) $(CFLAGS) benchCPU.cpp $(THREAD)
 
 mem: benchMem.o
-	$(CC) benchMem.o -o benchMem
+	$(CC) benchMem.o -o benchMem $(THREAD)
 
 benchMem.o: benchMem.cpp
-	$(CC) $(CFLAGS) benchMem.cpp
+	$(CC) $(CFLAGS) benchMem.cpp $(THREAD)
 
 clean:
 	rm -rf *.o benchCPU benchMem
--- benchMem.cpp
+++ benchMem.cpp
@@ -5,16 +5,17 @@
 const size_t B = 1;
 const size_t KB = 1024;
 const size_t MB = 1048576;
-const unsigned long NUMINTS = 2621440;
+const unsigned long NUMINTS = 2621440; //number of ints in 10MB
 
 int main(int argv, char* argc[]){
     clock_t begin, end, clicks;
     float secs;
     size_t size = 0;
-    int numLoops = 1;
-    int* p;
-    char* ptr1 = p;
+    int* p = new int;
+    char* ptr1 = (char*)p-4;
     char* ptr2;
+	char* endptr;
+	int numOps = 0;
 
     if(argv != 2){
         fprintf(stderr, "Please identify a block size (B, KB, or MB)");
@@ -28,26 +29,29 @@ int main(int argv, char* argc[]){
     }else if(strcmp(argc[1],"MB")==0){
         size = MB;
     }
-    for(int n=0; n<NUMINTS; n++){
+    for(int n=0; n<NUMINTS*20; n++){
         p = new int;
     }
-    ptr2 = (ptr1+p)/2;
+    ptr2 = ptr1+size;
+	endptr = (char*)((long)ptr1+200*MB);
+//	printf("ptr1=%p\nptr2=%p\np=%p\nendptr=%p\n",ptr1,ptr2,p,endptr);
 
     begin = clock();
-    for(int m=0; m<numLoops; m++){
-        for(unsigned int i=0; i<ARRAY_SIZE*5; i+=size){
+        while(ptr1<endptr){
             memcpy(ptr1, ptr2, size);
+//	printf("copying from %p to %p, operation number: %d\n",ptr1,ptr2,numOps);
             ptr1+=size;
             ptr2+=size;
+		numOps++;
         }
-    }
+    
     end = clock();
     clicks = end - begin;
     secs = ((float)clicks)/CLOCKS_PER_SEC;
 
-    printf("Copied 2MB %d times using size %s, took %f secs.\n",numLoops, argc[1], secs);
-    printf("Throughput: %f MB/Sec\n", (numLoops*ARRAY_SIZE)/(MB*secs));
-    printf("Latency per operation: %4.2f ms\n", (secs*1000)/(ARRAY_SIZE));   
+    printf("Copied 200MB using size %s, took %f secs.\nnumOps = %d\n", argc[1], secs, numOps);
+    printf("Throughput: %f MB/Sec\n", (10/secs));
+    printf("Latency per operation: %2.2f ms\n", numOps/(secs*1000));   
  
     return 0;
 }

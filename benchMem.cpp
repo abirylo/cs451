af6e9dec7544ab02d9b0c958231aefa746e5949b
Dan Hitchings <DannMan999@gmail.com>
2014-01-26 23:45:50 -0600

    Rewrote benchMem using malloc and free.  Also made the test a function for ease in threading.

--- benchMem.cpp
+++ benchMem.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
@@ -7,34 +8,24 @@ const size_t KB = 1024;
 const size_t MB = 1048576;
 const unsigned long NUMINTS = 2621440; //number of ints in 10MB
 
-int main(int argv, char* argc[]){
+struct results_t {
+    float throughput;
+    float latency;
+};
+
+results_t memTest(size_t size){
+    results_t results;
     clock_t begin, end, clicks;
     float secs;
-    size_t size = 0;
-    int* p = new int;
-    char* ptr1 = (char*)p-4;
-    char* ptr2;
-	char* endptr;
-	int numOps = 0;
+    char *ptr1, *ptr2, *endptr;
+    void* mem;
+    int numOps = 0;
 
-    if(argv != 2){
-        fprintf(stderr, "Please identify a block size (B, KB, or MB)");
-        return 1;
-    }
-    
-    if(strcmp(argc[1],"B")==0){
-        size = B;
-    }else if(strcmp(argc[1],"KB")==0){
-        size = KB;
-    }else if(strcmp(argc[1],"MB")==0){
-        size = MB;
-    }
-    for(int n=0; n<NUMINTS*20; n++){
-        p = new int;
-    }
+    mem = malloc(MB*100);
+    ptr1 = (char*)mem;
     ptr2 = ptr1+size;
-	endptr = (char*)((long)ptr1+200*MB);
-//	printf("ptr1=%p\nptr2=%p\np=%p\nendptr=%p\n",ptr1,ptr2,p,endptr);
+	endptr = (char*)((long)ptr1+100*MB);
+//	printf("ptr1=%p\nptr2=%p\nendptr=%p\n",ptr1,ptr2,endptr);
 
     begin = clock();
         while(ptr1<endptr){
@@ -48,10 +39,38 @@ int main(int argv, char* argc[]){
     end = clock();
     clicks = end - begin;
     secs = ((float)clicks)/CLOCKS_PER_SEC;
+    
+    results.throughput = 100/secs;
+    results.latency = numOps/(secs*1000);
 
-    printf("Copied 200MB using size %s, took %f secs.\nnumOps = %d\n", argc[1], secs, numOps);
-    printf("Throughput: %f MB/Sec\n", (10/secs));
-    printf("Latency per operation: %2.2f ms\n", numOps/(secs*1000));   
+    free(mem);
+        
+    return results;
+}
+
+
+int main(int argv, char* argc[]){
+    size_t size = 0;
+    results_t r;
+
+    if(argv != 2){
+        fprintf(stderr, "Please identify a block size (B, KB, or MB)");
+        return 1;
+    }
+    
+    if(strcmp(argc[1],"B")==0){
+        size = B;
+    }else if(strcmp(argc[1],"KB")==0){
+        size = KB;
+    }else if(strcmp(argc[1],"MB")==0){
+        size = MB;
+    }
+
+    for(int i=0; i<10; i++){
+        r = memTest(size);
+        printf("Throughput: %f MB/Sec\n", r.throughput);
+        printf("Latency per operation: %2.2f ms\n", r.latency);
+    }
  
     return 0;
 }
